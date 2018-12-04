9f9c5ad5b0291d4882708a006499281b04e25000
Amanda Chen <achen01@stuy.edu>
2018-12-04 12:43:38 -0500

    wrote selectionsort method

--- sorts.java
+++ sorts.java
@@ -1,3 +1,15 @@
 public class sorts {
-  
+  public static void selectionsort(int[] ary){
+    for (int i = 0; i < ary.length; i++){
+      int idxSmall = 0;
+      for (int j = i + 1; j < ary.length; j++){
+        if (ary[j] < small){
+          idxSmall = j;
+        }
+      }
+      int temp = ary[j];
+      ary[j] = ary[i];
+      ary[i] = temp;
+    }
+  }
 }
