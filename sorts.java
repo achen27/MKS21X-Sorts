fb35cd6a339e59e212f74547ae6d3498a29c1f70
Amanda Chen <achen01@stuy.edu>
2018-12-04 19:19:34 -0500

    fixed selectionsort method

--- sorts.java
+++ sorts.java
@@ -1,15 +1,28 @@
+import java.util.Arrays;
 public class sorts {
   public static void selectionsort(int[] ary){
-    for (int i = 0; i < ary.length; i++){
-      int idxSmall = 0;
-      for (int j = i + 1; j < ary.length; j++){
-        if (ary[j] < small){
+    for (int i = 0; i < ary.length - 1; i++){
+      System.out.println("Sorting: " + Arrays.toString(ary));
+      int idxSmall = i;
+      for (int j = i; j < ary.length; j++){
+        if (ary[j] < ary[idxSmall]){
           idxSmall = j;
         }
       }
-      int temp = ary[j];
-      ary[j] = ary[i];
+      int temp = ary[idxSmall];
+      ary[idxSmall] = ary[i];
       ary[i] = temp;
     }
+  }
+
+  public static void main(String[]args){
+    int length = Integer.parseInt(args[0]);
+    int[] list = new int[length];
+    for (int i = 0; i < list.length; i++){
+      list[i] = (int) (Math.random() * 100);
+    }
+    System.out.println("Start: " + Arrays.toString(list));
+    selectionsort(list);
+    System.out.println("End: " + Arrays.toString(list));
   }
 }
