027fa84eaa0ec174aece0d2457ed093eb3c4b6d8
Amanda Chen <achen01@stuy.edu>
2018-12-05 10:38:25 -0500

    tested selectionSort

--- Sorts.java
+++ Sorts.java
@@ -2,7 +2,6 @@ import java.util.Arrays;
 public class Sorts{
   public static void selectionSort(int[] ary){
     for (int i = 0; i < ary.length - 1; i++){
-      System.out.println("Sorting: " + Arrays.toString(ary));
       int idxSmall = i;
       for (int j = i; j < ary.length; j++){
         if (ary[j] < ary[idxSmall]){
@@ -12,6 +11,7 @@ public class Sorts{
       int temp = ary[idxSmall];
       ary[idxSmall] = ary[i];
       ary[i] = temp;
+      System.out.println("Sorting: " + Arrays.toString(ary));
     }
   }
 
@@ -25,11 +25,19 @@ public class Sorts{
   }
 
   public static void main(String[]args){
-    int length = Integer.parseInt(args[0]);
-    int[] list = new int[length];
-    for (int i = 0; i < list.length; i++){
+    //int length = Integer.parseInt(args[0]);
+    int[] list = new int[8];
+    /*for (int i = 0; i < list.length; i++){
       list[i] = (int) (Math.random() * 100);
-    }
+    }*/
+    list[0] = 5;
+    list[1] = 1;
+    list[2] = 12;
+    list[3] = -5;
+    list[4] = 16;
+    list[5] = 2;
+    list[6] = 12;
+    list[7] = 14;
     System.out.println("Start: " + Arrays.toString(list));
     selectionSort(list);
     System.out.println("End: " + Arrays.toString(list));
